94a525b04364180a6f49d20e601e0fd75f13fc38
haosdent <haosdent@gmail.com>
2013-03-02 01:18:47 +0800

    Change the type of edge value from Object to Generic.

--- src/main/scala/me/haosdent/cloud/graph/Service/Edges.scala
+++ src/main/scala/me/haosdent/cloud/graph/Service/Edges.scala
@@ -1,11 +1,25 @@
 package me.haosdent.cloud.graph.service
 
 import me.haosdent.cloud.graph.model.Edge
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.client.Result
 
 class Edges {
+
+  val cfBytes = Bytes.toBytes("cf")
+  val valueBytes = Bytes.toBytes("value")
+
   def getEdgeList(sourceId: Long) : List[Edge] = {
     //TODO
     List.empty[Edge]
+  }
+
+  def parse[T](record: Result, toValue: Array[Byte] => T) : Edge[T] = {
+    val rowkey = record.getRow
+    val sourceId = Bytes.toLong(rowkey, 0, 8)
+    val targetId = Bytes.toLong(rowkey, 8, 8)
+    val value = toValue(record.getValue(cfBytes, valueBytes))
+    new Edge(sourceId, targetId, value)
   }
 
   def save(edge: Edge) = {
--- src/main/scala/me/haosdent/cloud/graph/model/Edge.scala
+++ src/main/scala/me/haosdent/cloud/graph/model/Edge.scala
@@ -1,4 +1,4 @@
 package me.haosdent.cloud.graph.model
 
-class Edge(val sourceId: Long, val targetId: Long, val value: Object) {
+class Edge[T](val sourceId: Long, val targetId: Long, val value: T) {
 }
